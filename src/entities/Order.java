ffe6fc73efb107d3867e9ea1fb3bd67b2fbc5dac
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2018-09-04 22:34:54 -0400

    Entities completas

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -9,21 +9,23 @@ import entities.enums.OrderStatus;
 
 public class Order {
 	
-	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
 	private Date moment;
 	private OrderStatus status;
 	
-	
-	private List<OrderItem> products = new ArrayList<>();
+	private Client client;
+	private List<OrderItem> items = new ArrayList<>();
 	
 	public Order() {
 		
 	}
-
-	public Order(Date moment, OrderStatus status) {
+	
+	public Order(Date moment, OrderStatus status, Client client) {
+		super();
 		this.moment = moment;
 		this.status = status;
+		this.client = client;
 	}
 
 	public Date getMoment() {
@@ -42,19 +44,50 @@ public class Order {
 		this.status = status;
 	}
 
-	public List<OrderItem> getProducts() {
-		return products;
+	public Client getClient() {
+		return client;
 	}
-	
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getItems() {
+		return items;
+	}
+
 	public void addItem(OrderItem item) {
-		products.add(item);
+		items.add(item);
 	}
 	
 	public void removeItem(OrderItem item) {
-		products.remove(item);
+		items.remove(item);
 	}
 	
-
+	public double total() {
+		double sum = 0.0;
+		for(OrderItem item : items) {
+			sum += item.subTotal();
+		}
+		return sum;
+	}
 	
-
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for(OrderItem item : items) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $ ");
+		sb.append(String.format("%.2f", total()));
+		
+		return sb.toString();
+	}
 }
